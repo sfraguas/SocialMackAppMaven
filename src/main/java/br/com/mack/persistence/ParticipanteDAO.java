58e8cc927e72e5915b6ff7ef8eaedbab7c5a4690
Bruno Brito <brunoalbuquerquebrito@outlook.com>
2017-03-30 17:05:08 +0000

    Chage positon folder

--- src/main/java/br/com/mack/persistence/ParticipanteDAO.java
+++ src/main/java/br/com/mack/persistence/ParticipanteDAO.java
@@ -33,11 +33,14 @@ public class ParticipanteDAO implements GenericDAO<Participante> {
 
      @Override
     public void create(Participante participante) throws Exception {
+        et.begin();
         em.persist(participante);
+        et.commit();
     }
     
     @Override
     public List<Participante> readAll() throws Exception {
+    
         Query q = em.createQuery("SELECT p FROM Participante p");
         List<Participante> lista = q.getResultList();
         return lista;
@@ -45,23 +48,21 @@ public class ParticipanteDAO implements GenericDAO<Participante> {
     
     @Override
     public Participante readById(long id) throws Exception {
-        
         return em.find(Participante.class, id); //EAGER
 //        return em.getReference(Participante.class, id) //LAZY
     }
     
     @Override
     public void update(Participante participante) throws Exception {
+        et.begin();
         em.persist(em.merge(participante));
+        et.begin();
     }
     
     @Override
     public void delete(Participante participante) throws Exception {
+        et.begin();
         em.remove(em.merge(participante));
+        et.begin();
     }
-
-    public List<Participante> readById() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
