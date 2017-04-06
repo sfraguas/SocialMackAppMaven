a32317695fae2e4153464ce662211e93a967c190
Bruno Brito <brunoalbuquerquebrito@outlook.com>
2017-04-06 18:31:57 +0000

    teste getenv connection

--- src/main/java/br/com/mack/controller/impl/CadastroController.java
+++ src/main/java/br/com/mack/controller/impl/CadastroController.java
@@ -17,28 +17,29 @@ import java.lang.Exception;
 
 public class CadastroController extends AbstractController {
 
-    //ParticipanteDAO participanteDAO = new ParticipanteDAO();
+    ParticipanteDAO participanteDAO = new ParticipanteDAO();
     
 
     @Override
     public void execute() {
         System.out.println("FUNCIONOU");
-        
         //TesteConnection connection = new TesteConnection();
-        //String name = this.getRequest().getParameter("name");
-        //String email = this.getRequest().getParameter("email");
-        //Participante participante = new Participante();
-        //participante.setNome(name);
-        //participante.setEmail(email);
+        
+        String name = this.getRequest().getParameter("name");
+        String email = this.getRequest().getParameter("email");
+        Participante participante = new Participante();
+        participante.setNome(name);
+        participante.setEmail(email);
 
-        //getRequest().getSession().setAttribute("participante", participante);
-        /*try {
-            //participanteDAO.create(participante);
+        getRequest().getSession().setAttribute("participante", participante);
+        try {
+            participanteDAO.create(participante);
+            this.setReturnPage("sucesso.jsp");
         } catch (Exception ex) {
             Logger.getLogger(CadastroController.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
     
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        /*String dbUrl = System.getenv("JDBC_DATABASE_URL");
         if(dbUrl != null){
             Connection c = null;
             try{
@@ -51,6 +52,6 @@ public class CadastroController extends AbstractController {
                 getRequest().getSession().setAttribute("user",dbUrl+" \n Conectado com sucesso");
                 this.setReturnPage("user_area/lista_palestras.jsp");
             }
-        }
+        }*/
     }
 }
