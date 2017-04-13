a5b42e8e960704367dd2bf8ac3290d75d296afb5
Renan <renan.rsg@hotmail.com>
2017-04-13 16:53:24 +0000

    Exemplo de sendgrid

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -6,25 +6,24 @@ import java.io.IOException;
 public class EmailDispatcher {
         
     public static void sendEmail(String to) {
-        Email from = new Email("renan.databyte@gmail.com");
-        String subject = "Teste";
-        Email toMail = new Email(to);
-        Content content = new Content("text/plain", "E-mail de teste");
-        Mail mail = new Mail(from, subject, toMail, content);
-        
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-        Request request = new Request();
-        
-        try{
-            request.method = Method.POST;
-            request.endpoint = "mail/send";
-            request.body = mail.build();
-            Response response = sg.api(request);
-            System.out.println(response.statusCode);
-            System.out.println(response.body);
-            System.out.println(response.headers);
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
+        Email from = new Email("brunoalbuquerquebrito@outlook.com");
+    String subject = "Hello World from the SendGrid Java Library!";
+    Email toMail = new Email(to);
+    Content content = new Content("text/plain", "Hello, Email!");
+    Mail mail = new Mail(from, subject, toMail, content);
+
+    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+    Request request = new Request();
+    try {
+      request.method = Method.POST;
+      request.endpoint = "mail/send";
+      request.body = mail.build();
+      Response response = sg.api(request);
+      System.out.println(response.statusCode);
+      System.out.println(response.body);
+      System.out.println(response.headers);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
     }
 }
