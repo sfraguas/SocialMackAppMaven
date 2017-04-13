1a676114d63f7d46c32d458574bc6e453c3aaf17
Renan <renan.rsg@hotmail.com>
2017-04-13 22:28:54 +0000

    Tentando enviar e-mail sem EmailHerlper

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -6,25 +6,18 @@ import java.io.IOException;
 public class EmailDispatcher {
         
     public static void sendEmail(String to) {
-        System.out.println("Arrived e-mail: " + to);
-        Email from = new Email("renan.rsg@hotmail.com");
-        String subject = "Hello World from the SendGrid Java Library!";
-        Email toMail = new Email(to);
-        Content content = new Content("text/plain", "Hello, Email!");
-        Mail mail = new Mail(from, subject, toMail, content);
-
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-        Request request = new Request();
         try {
-            request.method = Method.POST;
-            request.endpoint = "mail/send";
-            request.body = mail.build();
-            Response response = sg.api(request);
-            System.out.println(response.statusCode);
-            System.out.println(response.body);
-            System.out.println(response.headers);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+      Request request = new Request();
+      request.method = Method.POST;
+      request.endpoint = "mail/send";
+      request.body = "{\"personalizations\":[{\"to\":[{\"email\":\"" + to + "\"}],\"subject\":\"Hello World from the SendGrid Java Library!\"}],\"from\":{\"email\":\"renan.rsg@hotmail.com\"},\"content\":[{\"type\":\"text/plain\",\"value\": \"Hello, Email!\"}]}";
+      Response response = sg.api(request);
+      System.out.println(response.statusCode);
+      System.out.println(response.body);
+      System.out.println(response.headers);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
     }
 }
