80249ef5c1692185bc7a54505d0c8ad970c016df
Renan <renan.rsg@hotmail.com>
2017-04-13 16:26:16 +0000

    Exemplo de sendgrid

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -1,12 +1,30 @@
 package br.com.mack.email;
 
-
-    public class EmailDispatcher {
-                
-        private static final String FROM_EMAIL = "renan.databyte@gmail.com";
-        private static final String FROM_PASSWORD = "vermelhodb";
+import com.sendgrid.*;
+import java.io.IOException;
+    
+public class EmailDispatcher {
         
-        public static void sendEmail(String to) {
-           
+    public static void sendEmail(String to) {
+        Email from = new Email("renan.databyte@gmail.com");
+        String subject = "Teste";
+        Email toMail = new Email(to);
+        Content content = new Content("text/plain", "E-mail de teste");
+        Mail mail = new Mail(from, subject, toMail, content);
+        
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        
+        try{
+            request.method = Method.POST;
+            request.endpoint = "main/send";
+            request.body = mail.build();
+            Response response = sg.api(request);
+            System.out.println(response.statusCode);
+            System.out.println(response.body);
+            System.out.println(response.headers);
+        }catch(IOException ex){
+            ex.printStackTrace();
         }
     }
+}
