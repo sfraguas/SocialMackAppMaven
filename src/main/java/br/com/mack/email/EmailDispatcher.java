7c261d2527d51f525e11fb28b5059c6eea40df1d
Yuri <iuri_novaes.2010@hotmail.com>
2017-04-14 21:23:09 +0000

    teste com email

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -2,28 +2,30 @@ package br.com.mack.email;
 
 import com.sendgrid.*;
 import java.io.IOException;
-    
-public class EmailDispatcher {
-        
-    public static void sendEmail(String to) {
-        Email from = new Email("renan.rsg@hotmail.com");
-        String subject = "Hello World from the SendGrid Java Library!";
-        Email toMail = new Email("renan.databyte@gmail.com");
-        Content content = new Content("text/plain", "Hello, Email!");
-        Mail mail = new Mail(from, subject, toMail, content);
 
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-        Request request = new Request();
-        try {
-            request.method = Method.POST;
-            request.endpoint = "mail/send";
-            request.body = mail.build();
-            Response response = sg.api(request);
-            System.out.println(response.statusCode);
-            System.out.println(response.body);
-            System.out.println(response.headers);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+    public class EmailDispatcher {
+                
+
+        public static void sendEmail(String to) {
+            Email from = new Email("yuri.novaes.2013@gmail.com");
+            
+            String subject = "Hello World from the SendGrid Java Library!";
+            Email email = new Email(to);
+            Content content = new Content("text/plain", "Hello, Email!");
+            Mail mail = new Mail(from, subject, email, content);
+            
+            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+            Request request = new Request();
+                try {
+                  request.method = Method.POST;
+                  request.endpoint = "mail/send";
+                  request.body = mail.build();
+                  Response response = sg.api(request);
+                  System.out.println(response.statusCode);
+                  System.out.println(response.body);
+                  System.out.println(response.headers);
+                } catch (IOException ex) {
+                  ex.printStackTrace();
+                }
         }
     }
-}
