4dda06fa0f03ab3f111a5dca47349ed314e3a294
Yuri <iuri_novaes.2010@hotmail.com>
2017-04-14 13:29:16 +0000

    Testando envio de email

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -1,39 +1,31 @@
 package br.com.mack.email;
 
-
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
-
+import com.sendgrid.*;
+import java.io.IOException;
 
     public class EmailDispatcher {
                 
-        private static final String FROM_EMAIL = "renan.databyte@gmail.com";
-        private static final String FROM_PASSWORD = "vermelhodb";
-        
+
         public static void sendEmail(String to) {
-            SimpleEmail email = new SimpleEmail();
-            try {
-                email.setDebug(true);
-                email.setHostName("smtp.gmail.com");
-                email.setSmtpPort(465);
-                email.addTo(to, "Yuri");
-                email.setFrom(FROM_EMAIL, "Renan");
-                email.setSubject("Geração de Senha");
-                email.setMsg("Senha para consumo externo");
-                email.setSSL(true);
-                email.setAuthentication(FROM_EMAIL, FROM_PASSWORD);
-                email.send();
-                //email.setAuthenticator(new DefaultAuthenticator(FROM_EMAIL, FROM_PASSWORD));
-                //email.setSSL(true);
-                //email.setFrom(FROM_EMAIL);
-                //email.setSubject("Senha automatica");
-               // email.setMsg("codigo que a gente vai gerar");
-                //email.addTo("iuri_novaes.2010@hotmail.com");
-                //email.getMailSession().getProperties().put("mail.smtp.auth", "true");
-                //email.send();
-            } catch (EmailException e) {
-                    e.printStackTrace();
-                }   
+            Email from = new Email("brunoalbuquerquebrito@outlook.com");
+            
+            String subject = "Hello World from the SendGrid Java Library!";
+            Email toMail = new Email(to);
+            Content content = new Content("text/plain", "Hello, Email!");
+            Mail mail = new Mail(from, subject, toMail, content);
+            
+            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+            Request request = new Request();
+                try {
+                  request.method = Method.POST;
+                  request.endpoint = "mail/send";
+                  request.body = mail.build();
+                  Response response = sg.api(request);
+                  System.out.println(response.statusCode);
+                  System.out.println(response.body);
+                  System.out.println(response.headers);
+                } catch (IOException ex) {
+                  ex.printStackTrace();
+                }
         }
     }
