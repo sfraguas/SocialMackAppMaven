99a348586686e3c9a0c8a1f79408454d27c9cf6e
Yuri <iuri_novaes.2010@hotmail.com>
2017-04-15 03:15:07 +0000

    agora sim vai o email

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -10,11 +10,17 @@ import java.io.IOException;
         
             try {
               SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+              
               Request request = new Request();
+              
               request.method = Method.POST;
+              
               request.endpoint = "mail/send";
+              
               request.body = "{\"personalizations\":[{\"to\":[{\"email\":\"iuri_novaes.2010@hotmail.com\"}],\"subject\":\"Hello World from Mackenzie!\"}],\"from\":{\"email\":\"yuri.novaes.2013@gmail.com\"},\"content\":[{\"type\":\"text/plain\",\"value\": \"Hello, Email!\"}]}";
+              
               Response response = sg.api(request);
+              
             } catch (IOException ex) {
               ex.printStackTrace();
             }
