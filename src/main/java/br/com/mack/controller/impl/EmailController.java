f8d5af7acd1028af7fd9f7134c4a4e3d8f701665
Yuri <iuri_novaes.2010@hotmail.com>
2017-04-15 03:05:15 +0000

    agora vai o email

--- src/main/java/br/com/mack/controller/impl/EmailController.java
+++ src/main/java/br/com/mack/controller/impl/EmailController.java
@@ -1,14 +1,14 @@
 package br.com.mack.controller.impl;
- -
- -import br.com.mack.controller.AbstractController;
- -import br.com.mack.email.EmailDispatcher;
- -
- -public class EmailController extends AbstractController{
- -    
- -    
- -    @Override
- -    public void execute() {
- -        String email = this.getRequest().getParameter("email");
- -        EmailDispatcher.sendEmail(email);
- -    }
- -} 
+ 
+ import br.com.mack.controller.AbstractController;
+ import br.com.mack.email.EmailDispatcher;
+ 
+ public class EmailController extends AbstractController{
+     
+     
+     @Override
+     public void execute() {
+         String email = this.getRequest().getParameter("email");
+         EmailDispatcher.sendEmail(email);
+     }
+ } 
--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -8,26 +8,16 @@ import java.io.IOException;
 
         public static void sendEmail(String email) {
         
-             Email from = new Email("iuri_novaes.2010@hotmail.com");
-             
-                String subject = "Hello World from Mackenzie!";
-                
-                Email to = new Email(email);
-                
-                Content content = new Content("text/plain", "Hello, Email!");
-                
-                Mail mail = new Mail(from, subject, to, content);
-            
-                SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-                Request request = new Request();
-                try {
-                  request.method = Method.POST;
-                  request.endpoint = "mail/send";
-                  request.body = mail.build();
-                  Response response = sg.api(request);
-                } catch (IOException ex) {
-                  ex.printStackTrace();
-                }
+            try {
+              SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+              Request request = new Request();
+              request.method = Method.POST;
+              request.endpoint = "mail/send";
+              request.body = "{\"personalizations\":[{\"to\":[{\"email\":\"iuri_novaes.2010@hotmail.com\"}],\"subject\":\"Hello World from Mackenzie!\"}],\"from\":{\"email\":\"yuri.novaes.2013@gmail.com\"},\"content\":[{\"type\":\"text/plain\",\"value\": \"Hello, Email!\"}]}";
+              Response response = sg.api(request);
+            } catch (IOException ex) {
+              ex.printStackTrace();
+            }
   
         }
     }
