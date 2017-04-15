cc021cf9e6959fb77a1ac5b2c75c651025044384
Renan <renan.rsg@hotmail.com>
2017-04-15 18:09:54 +0000

    Correções na classe EmailDispatcher

--- src/main/java/br/com/mack/email/EmailDispatcher.java
+++ src/main/java/br/com/mack/email/EmailDispatcher.java
@@ -8,23 +8,25 @@ import java.io.IOException;
 
         public static void sendEmail(String email) {
         
-                Email from = new Email("yuri.novaes.2013@gmail.com");
-                String subject = "Testando SendGrid";
-                Email to = new Email(email);
-                Content content = new Content("text/plain", "Testando email com Send Grid");
-                Mail mail = new Mail(from, subject, to, content);
-        
-                try {
-                    SendGrid sg = new SendGrid("SENDGRID_API_KEY");
-                    Request request = new Request();
-                    request.method = Method.POST;
-                    request.endpoint = "mail/send";
-                    request.body = mail.build();
-                    Response response = sg.api(request);
-                }
-                catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+            Email from = new Email("test@example.com");
+            String subject = "Hello World from the SendGrid Java Library!";
+            Email to = new Email(email);
+            Content content = new Content("text/plain", "Hello, Email!");
+            Mail mail = new Mail(from, subject, to, content);
+
+            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+            Request request = new Request();
+            try {
+                request.method = Method.POST;
+                request.endpoint = "mail/send";
+                request.body = mail.build();
+                Response response = sg.api(request);
+                System.out.println(response.statusCode);
+                System.out.println(response.body);
+                System.out.println(response.headers);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
   
         }
     }
